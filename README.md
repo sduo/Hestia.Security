3171470c4f652bca51d0dbf747d7ae0cf72563c4
suduo <sduo@vip.qq.com>
2022-05-07 11:45:09 +0800

    init

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 [![](https://github.com/sduo/Hestia.Security/actions/workflows/main.yml/badge.svg)](https://github.com/sduo/Hestia.Security)
 [![](https://img.shields.io/nuget/v/Hestia.Security.svg)](https://www.nuget.org/packages/Hestia.Security)
+
+---
+# AES
+
+## AES128_CBC_PKCS7
+
+* KEY：16 位
+* IV：16 位
+
+```csharp
+//加密 
+byte[] encrypted = Utility.AES128_CBC_PKCS7_ENCRYPT(byte[] key,byte[] iv,byte[] decrypted);
+//解密
+byte[] decrypted = Utility.AES128_CBC_PKCS7_ENCRYPT(byte[] key,byte[] iv,byte[] encrypted);
+```
+
+
---
+++ src/AES/AES128_CBC_PKCS7.cs
@@ -0,0 +1,25 @@
+﻿using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.Paddings;
+using Org.BouncyCastle.Crypto.Parameters;
+
+namespace Hestia.Security
+{
+    public static partial class Utility
+    {
+        public static byte[] AES128_CBC_PKCS7_DECRYPT(byte[] key, byte[] iv, byte[] input) => AES128_CBC_PKCS7(false, key, iv, input);
+
+        public static byte[] AES128_CBC_PKCS7_ENCRYPT(byte[] key, byte[] iv, byte[] input) => AES128_CBC_PKCS7(true, key, iv, input);
+
+        private static byte[] AES128_CBC_PKCS7(bool encrypt, byte[] key, byte[] iv, byte[] input)
+        {
+            AesEngine engine = new();
+            CbcBlockCipher bc = new(engine);
+            PaddedBufferedBlockCipher pbc = new(bc, new Pkcs7Padding());
+            KeyParameter kp = new(key);
+            ParametersWithIV parameters = new(kp, iv);
+            pbc.Init(encrypt, parameters);
+            return pbc.DoFinal(input);
+        }
+    }
+}
